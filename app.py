bb8ecc1712d8ac1f9df23d9889b503185cb39626
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-19 07:31:21 -0500

    render template (& get HTML page of inputted URL)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -11,15 +12,25 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-
 
 if __name__ == '__main__':
     app.run()
