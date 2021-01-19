9fac5ab41f3e69f6465faa3a11c08939f089825c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-19 05:04:14 -0500

    add auth to local db URI connection

--- setup.bat
+++ setup.bat
@@ -1,5 +1,4 @@
 set APP_SETTINGS=config.DevelopmentConfig
-set DATABASE_URL=postgresql:///python_text_db
+set DATABASE_URL=postgresql://postgres:password@localhost:5432/python_text_db
 echo "Settings: %APP_SETTINGS% && DB_URL: %DATABASE_URL%"
-
 .\env\scripts\activate
